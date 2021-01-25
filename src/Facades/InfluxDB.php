878bbe009c1ccf5698e7b179289770acba4345c5
Alex Modis <modis.alex@gmail.com>
2021-01-25 15:36:06 +0200

    Init Repo

---
+++ src/Facades/InfluxDB.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Modis\InfluxDB\Facades;
+
+use Illuminate\Support\Facades\Facade as LaravelFacade;
+use InfluxDB\Database;
+
+class InfluxDB extends LaravelFacade
+{
+    /**
+     * Get the registered name of the component.
+     *
+     * @return string
+     */
+    protected static function getFacadeAccessor()
+    {
+        return Database::class;
+    }
+}
